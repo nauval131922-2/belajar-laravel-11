ee4f94b8bd667a422a9123b925a914ed2879184d
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-05-01 10:21:29 +0700

    Belajar Laravel 11 | 5. Blade Component

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Contact</title>
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog">Blog</a>
-    <a href="/contact">Contact</a>
-    <h1>Halaman Contact</h1>
-    <ul>
-        <li>
-            Instagram: <a href="https://www.instagram.com/">@instagram</a>
-        </li>
-        <li>
-            Twitter: <a href="https://twitter.com/">@twitter</a>
-        </li>
-        <li>
-            Youtube: <a href="https://www.youtube.com/">@youtube</a>
-        </li>
-    </ul>
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Ini adalah Contact Page</h3>
+</x-layout>
