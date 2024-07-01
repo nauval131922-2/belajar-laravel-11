4a1588958d1f5c5a5d746d72f6f4fd6fa8b53c0c
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-07-01 15:13:55 +0700

    Belajar Laravel 11 | 9. Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,37 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Nauval Gunawan',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum recusandae neque pariatur consectetur, unde error, tempore dolorum eius quasi explicabo, voluptatibus voluptates sapiente sit expedita harum iusto ducimus ad? Voluptatibus!'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Nauval Gunawan',
-                'body' => 'Lorem 2 ipsum dolor sit amet consectetur adipisicing elit. Eum recusandae neque pariatur consectetur, unde error, tempore dolorum eius quasi explicabo, voluptatibus voluptates sapiente sit expedita harum iusto ducimus ad? Voluptatibus!'
-            ]
-        ];
-    }
+    use HasFactory;
 
-    public static function find($slug): array {
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if (!$post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    protected $guarded = [];
 }
