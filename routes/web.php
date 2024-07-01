8c0065619f951b6aaba660328670f6bf61d7bc00
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-07-01 10:59:23 +0700

    Belajar Laravel 11 | 6. View Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,48 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Nauval Gunawan', 'title' => 'About Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Nauval Gunawan',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum recusandae neque pariatur consectetur, unde error, tempore dolorum eius quasi explicabo, voluptatibus voluptates sapiente sit expedita harum iusto ducimus ad? Voluptatibus!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Nauval Gunawan',
+            'body' => 'Lorem 2 ipsum dolor sit amet consectetur adipisicing elit. Eum recusandae neque pariatur consectetur, unde error, tempore dolorum eius quasi explicabo, voluptatibus voluptates sapiente sit expedita harum iusto ducimus ad? Voluptatibus!'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Nauval Gunawan',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum recusandae neque pariatur consectetur, unde error, tempore dolorum eius quasi explicabo, voluptatibus voluptates sapiente sit expedita harum iusto ducimus ad? Voluptatibus!'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Nauval Gunawan',
+            'body' => 'Lorem 2 ipsum dolor sit amet consectetur adipisicing elit. Eum recusandae neque pariatur consectetur, unde error, tempore dolorum eius quasi explicabo, voluptatibus voluptates sapiente sit expedita harum iusto ducimus ad? Voluptatibus!'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
