cf15bbe464a28f812aa7966932581f1aad0dde8f
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-07-02 08:19:52 +0700

    Belajar Laravel 11 | 12. Post Category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,11 +6,11 @@
             <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
                 <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
             </a>
-            <div class="text-base text-gray-500">
+            <div>
                 By
-                <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a>
+                <a href="/authors/{{ $post->author->username }}" class="text-base text-gray-500 hover:underline">{{ $post->author->name }}</a>
                 in
-                <a href="#" class="hover:underline">Web Programming</a> | {{ $post['created_at']->diffForHumans() }}
+                <a href="/categories/{{ $post->category->slug }}" class="text-base text-gray-500 hover:underline">{{ $post->category->name }}</a> | {{ $post['created_at']->diffForHumans() }}
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 150) }}</p>
             <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
